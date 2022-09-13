5518b4bd79c30a5041bd614309c96e0f79ca4c5f
Teshie <teshomeaychiluhem@gmail.com>
2022-09-13 06:16:00 -0700

    setup router

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,12 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import Department from './Department';
 import NxWelcome from './nx-welcome';
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Employee from './Employee';
+import Navigation from './Department';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="web" />
-      <div />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route path="employees" element={<Employee />}></Route>
+          <Route path="departments" element={<Employee />}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
