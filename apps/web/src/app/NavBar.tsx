c249c9aa6771b5f3be1e39b6f51beec74bf816f4
Teshie <teshomeaychiluhem@gmail.com>
2022-09-14 02:10:09 -0700

    navbar done

--- apps/web/src/app/NavBar.tsx
+++ apps/web/src/app/NavBar.tsx
@@ -5,15 +5,14 @@ export default function Navigation() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-500 mb-3">
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-900 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
-            >
-              Tensor System
-            </a>
+            <img
+              src="https://www.tensorsystems.net/images/tensor-systems.png"
+              alt="tensor"
+              width={300}
+            />
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
